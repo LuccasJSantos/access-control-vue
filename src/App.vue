db40c0e613bed1572a369c02e0b56842b2f3975a
LuccasJSantos <luccasjsantos@hotmail.com>
2021-11-09 13:30:42 -0300

    feat(root): add app top bar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,37 @@
 <template>
   <v-app id="app">
-    <v-app-bar>
+    <v-app-bar app dark color="primary">
       <v-app-bar-nav-icon />
 
       <v-toolbar-title>Current route</v-toolbar-title>
+
+      <v-spacer />
+
+      <v-menu offset-y>
+        <template v-slot:activator="{ on, attrs }">
+          <v-avatar size="52">
+            <v-img
+              v-bind="attrs"
+              v-on="on"
+              src="https://images.unsplash.com/photo-1545167622-3a6ac756afa4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=712&q=80"
+              alt="Someone"
+            />
+          </v-avatar>
+        </template>
+
+        <v-list dense>
+          <v-list-item link color="error">
+            <v-list-item-icon>
+              <v-icon color="error">mdi-logout</v-icon>
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title class="error--text">Sair</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
+
     <router-view />
   </v-app>
 </template>
