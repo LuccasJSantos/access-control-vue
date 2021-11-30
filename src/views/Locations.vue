579396433d9ae7ac97cfecfecdbcda964bccdeca
LuccasJSantos <luccasjsantos@hotmail.com>
2021-11-30 01:49:46 -0300

    feat(component): add roles column to the Locations view component

--- src/views/Locations.vue
+++ src/views/Locations.vue
@@ -8,7 +8,7 @@
       group-by="Mapa"
     >
       <template v-slot:group.header="{ group }">
-        <td colspan="3">
+        <td colspan="4">
           <div class="d-flex align-center justify-space-between">
             <span class="subtitle-2">{{ group }}</span>
 
@@ -17,7 +17,7 @@
               class="success"
               small
               v-if="!addingSensor.map"
-              @click="showAddSensor(group)"
+              @click="showAddLocation(group)"
             >
               <v-icon small color="white">mdi-plus</v-icon>
             </v-btn>
@@ -33,20 +33,60 @@
         </div>
       </template>
 
+      <template v-slot:item.roles="{ item }">
+        <div class="d-flex chips-gap">
+          <v-chip
+            v-for="(role, i) in item.roles"
+            :key="i"
+            color="secondary"
+            small
+          >
+            {{ role }}
+          </v-chip>
+        </div>
+      </template>
+
       <template v-slot:group.summary="{ group }">
         <td class="text-right" v-if="group === addingSensor.map">
           <span class="grey--text">###</span>
         </td>
 
         <td v-if="group === addingSensor.map">
-          <v-text-field v-model="addingSensor.locationName"></v-text-field>
+          <v-text-field
+            v-model="addingSensor.locationName"
+            class="body-2"
+          ></v-text-field>
+        </td>
+
+        <td v-if="group === addingSensor.map">
+          <v-combobox
+            v-model="addingSensor.RFID"
+            :items="['010', '017', '023', '025']"
+            multiple
+            chips
+            dense
+            style="max-width: 300px"
+          >
+            <template v-slot:selection="data">
+              <v-chip small color="success" :key="JSON.stringify(data.item)">
+                {{ data.item }}
+              </v-chip>
+            </template>
+          </v-combobox>
         </td>
 
         <td v-if="group === addingSensor.map">
           <div class="d-flex align-center justify-space-between">
             <v-combobox
-              v-model="addingSensor.RFID"
-              :items="['010', '017', '023', '025']"
+              v-model="addingSensor.roles"
+              :items="[
+                'Aluno Robótica',
+                'Aluno ADS',
+                'Aluno Eletrônica',
+                'Funcionário',
+                'Professor',
+                'Coordenação',
+              ]"
               multiple
               chips
               dense
@@ -59,9 +99,9 @@
               </template>
             </v-combobox>
 
-            <v-btn depressed color="success" @click="addSensor"
-              >Adicionar</v-btn
-            >
+            <v-btn icon class="ml-5 success" small @click="addLocation">
+              <v-icon small color="white">mdi-plus</v-icon>
+            </v-btn>
           </div>
         </td>
       </template>
@@ -78,6 +118,7 @@ export default {
         map: null,
         locationName: "",
         RFID: [],
+        roles: [],
       },
       headers: [
         {
@@ -99,6 +140,12 @@ export default {
           filterable: false,
           value: "RFID",
         },
+        {
+          text: "Funções",
+          align: "start",
+          filterable: true,
+          value: "roles",
+        },
       ],
       locations: [
         {
@@ -106,45 +153,51 @@ export default {
           Mapa: "Prédio 1º andar",
           name: "Laboratório de ADS",
           RFID: ["001"],
+          roles: ["Aluno ADS", "Funcionário", "Professor"],
         },
         {
           id: "955",
           Mapa: "Prédio 1º andar",
           name: "Laboratório de Eletrônica",
           RFID: ["001"],
+          roles: ["Aluno Eletrônica", "Funcionário", "Professor"],
         },
         {
           id: "452",
           Mapa: "Prédio 2º andar",
           name: "Laboratório de ADS 2",
           RFID: ["002", "003"],
+          roles: ["Aluno ADS", "Funcionário", "Professor"],
         },
         {
           id: "530",
-          Mapa: "Prédio 1º andar",
-          name: "Laboratório de Eletrônica",
-          RFID: ["005", "007", "011"],
+          Mapa: "Prédio 2º andar",
+          name: "Sala de Reuniões",
+          RFID: ["008", "013"],
+          roles: ["Professor", "Coordenação"],
         },
       ],
       sensors: [],
     };
   },
   methods: {
-    showAddSensor(group) {
+    showAddLocation(group) {
       this.addingSensor.map = group;
     },
-    addSensor() {
+    addLocation() {
       this.locations.push({
         id: parseInt(Math.random() * 999),
         Mapa: this.addingSensor.map,
         name: this.addingSensor.locationName,
         RFID: this.addingSensor.RFID,
+        roles: this.addingSensor.roles,
       });
 
       this.addingSensor = {
         map: null,
         locationName: "",
         RFID: [],
+        roles: [],
       };
     },
   },
